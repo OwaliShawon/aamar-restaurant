dc992f4b6b0ebcf141cefa29741c9bddab5e472a
Owali Ullah Shawon <124124094+owali-softic@users.noreply.github.com>
2024-07-08 10:36:48 +0600

    [update] root test case

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello, Amar Restaurant!"', () => {
+      expect(appController.getHello()).toBe('Hello, Amar Restaurant!');
     });
   });
 });
